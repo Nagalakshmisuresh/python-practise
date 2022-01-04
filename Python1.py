0377acd916c907a4837ca0eb2d8e5b629c0b3abc
Nagalakshmisuresh <93209339+Nagalakshmisuresh@users.noreply.github.com>
2022-01-04 11:20:12 +0530

    Add files via upload

---
+++ Python1.py
@@ -0,0 +1,7 @@
+
+# Write a Python program which prints the following as its output –
+#Welcome to Python programming.
+#It is a great language to learn and so simple. Let us have FUN and learn.
+print("welcome to pythonprogramming")
+print("It is a great language to learn and so simple.Let us have FUN and learn")
+
---
+++ Python2.py
@@ -0,0 +1,10 @@
+# Have a look at the below given Python code –
+#print("Welcome to Python programming.")
+#print("It is a great language to learn and so simple.") print("Let us have FUN and learn.")
+
+#Expected Output: (Should be printed in a Single line)
+#Welcome to Python programming. It is a great language to learn and so simple. Let us have FUN and learn.
+print("welcome to python programming.",end='\t')
+print("It is a great language to learn and so simple.",end='\t')
+print("Let us have FUN and learn")
+
