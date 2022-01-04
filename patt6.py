b238f268739a0728c64a5316f0b5c1b50076445b
Nagalakshmisuresh <93209339+Nagalakshmisuresh@users.noreply.github.com>
2022-01-04 14:43:50 +0530

    Add files via upload

---
+++ patt6.py
@@ -0,0 +1,6 @@
+n=10
+for i in range(5):
+    n=n+1
+    for j in range(i):
+        print(n,end=" ")
+    print()
---
+++ patt7.py
@@ -0,0 +1,7 @@
+n=10
+for i in range(5):
+    c=n+1
+    for j in range(i):
+        n=n+1
+        print(n,end=" ")
+    print()
